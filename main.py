22caf5705195d367b643868668399bfa837c6c94
Balas47 <68131412+Balas47@users.noreply.github.com>
2021-02-12 15:35:30 -0500

    Implemented simple player movement
    Small changes to the init function so that the player keeps track of a single surface that is updated throughout the game.
    Limitations to prevent the player from going off screen.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ WHITE = 8
 
 # The player class controls the position of the player on the screen, as
 # well as how the different inputs effect what happens for the player.
-class Player:
+class Player():
 
     def __init__(self, screen):
 
@@ -28,8 +28,46 @@ class Player:
         self.height = 10
         self.color = COLORS[VIOLET]
 
-        # Initial position of the player
+        # Initial position and speed of the player
         self.x, self.y = 0, 0
+        self.x_speed, self.y_speed = .5, .5
+
+        # Keep track of the player surface
+        self.player_surf = pygame.Surface((self.width, self.height))
+        self.player_surf.fill(self.color)
+
+
+    def move(self, buttons):
+        """
+        This function will take care of the movement of the player
+        :param buttons: The buttons the user pressed to determine direction
+                       of the player movement
+        """
+
+        # Take care of the actual movement of the player
+        if buttons[pygame.K_w]:
+            self.y -= self.y_speed
+            
+        if buttons[pygame.K_a]:
+            self.x -= self.x_speed
+
+        if buttons[pygame.K_s]:
+            self.y += self.y_speed
+
+        if buttons[pygame.K_d]:
+            self.x += self.x_speed
+
+        # Make sure that the player cannot go off of the screen
+        if 0 > self.x:
+            self.x = 0
+        elif self.x > self.screen.get_width() - self.width:
+            self.x -= self.x_speed
+
+        if 0 > self.y:
+            self.y = 0
+        elif self.y > self.screen.get_height() - self.height:
+            self.y -= self.y_speed
+
 
     def draw(self, dim_x, dim_y):
         """
@@ -39,11 +77,8 @@ class Player:
         :param dim_y: The y dimension of the screen.
         """
         
-        player_surf = pygame.Surface((self.width, self.height))
-        player_surf.fill(self.color)
-
         # For now, drawing the player onto the center of the screen
-        self.screen.blit(player_surf, (self.x, self.y))
+        self.screen.blit(self.player_surf, (self.x, self.y))
     
 
 if __name__ == "__main__":
@@ -61,14 +96,19 @@ if __name__ == "__main__":
     run_game = True
     while run_game:
 
-        # Quit the game when the user wants to
         for event in pygame.event.get():
+
+            # Stop the main loop if necessary
             if event.type == pygame.QUIT:
                 run_game = False
 
             # The escape key quits the game
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 run_game = False
+
+        # Take care of user inputs
+        keys = pygame.key.get_pressed()
+        the_player.move(keys)
 
         # Print everything on the screen
         screen.fill(COLORS[ORANGE])
