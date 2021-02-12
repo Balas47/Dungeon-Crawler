e3896b9ca9626647177e989ca36bf34a0ca62875
Balas47 <68131412+Balas47@users.noreply.github.com>
2021-02-12 14:37:25 -0500

    Setup game window, and list of simple color values

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 import sys, pygame
 
+#Color values for the rainbow in ROYGBIV, black, and white order
+COLORS = [(255, 0, 0), (255, 127, 0), (255, 255, 0), (0, 255, 0),
+          (0, 0, 255), (46, 43, 95), (139, 0, 255), (0, 0, 0), (255, 255, 255)]
+
+# Constants for accessing the color values
+RED = 0
+ORANGE = 1
+YELLOW = 2
+GREEN = 3
+BLUE = 4
+INDIGO = 5
+VIOLET = 6
+BLACK = 7
+WHITE = 8
+
 if __name__ == "__main__":
 
-    print("Hello World")
+    pygame.init()
+
+    # Setting up the game window
+    width = 500
+    height = 500
+    screen = pygame.display.set_mode([width, height])
+
+    # Setting up the main loop
+    run_game = True
+    while run_game:
+
+        # Quit the game when the user wants to
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run_game = False
+
+            # The escape key quits the game
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                run_game = False
+
+        screen.fill(COLORS[ORANGE])
+        pygame.display.flip()
+
+    pygame.quit()
